fb83f916d7ece479decb0288220d3500c735d541
Juan Antonio <juan.acv@gmail.com>
2015-05-28 19:32:50 +0200

    doesn't work if the query string is empty

--- app/main.js
+++ app/main.js
@@ -9,19 +9,23 @@ define(function (require) {
 	//Event triggered when someone pushes the search button
 	search_button.addEventListener("click", function(e) {
 		var query = document.getElementById("query").value;
-		//Get the results form Google Search Api and put them formatted in the web page
-    	messages.getData({callback:
-    		function(response) { 
-    			if (response=="error") { 
-    				document.getElementById("itembox").style.display = "block"
-    				var itembox = document.getElementById("itembox");
-    				itembox.innerHTML = '<li><h4>Unable to connect to API server</h4>';
-    			}
-    			else {
-    				layout.printIt(response, 1, 1);
-    				document.getElementById("imagebox").style.display = "block";
-    				document.getElementById("itembox").style.display = "block"	
-    		   }
-    		}, query:query, startIndex:0});
+			//Check if there is a query string
+			if (query != "") {
+				//Get the results form Google Search Api and put them formatted in the web page
+    			messages.getData({callback:
+    				function(response) { 
+    					if (response=="error") { 
+    						document.getElementById("itembox").style.display = "block"
+    						var itembox = document.getElementById("itembox");
+    						itembox.innerHTML = '<li><h4>Unable to connect to API server</h4>';
+    					}
+    					else {
+    						layout.printIt(response, 1, 1);
+    						document.getElementById("imagebox").style.display = "block";
+    						document.getElementById("itembox").style.display = "block"	
+    		   			}
+    				}, query:query, startIndex:0});
+    		}
     	});
+    	
 });
