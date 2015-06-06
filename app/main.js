294455b8bc018f2274fbe7a6d00737fcee97885a
Juan Antonio <juan.acv@gmail.com>
2015-06-06 20:26:51 +0200

    getPage now is used in main

--- app/main.js
+++ app/main.js
@@ -1,34 +1,10 @@
-define(["ajaxcall", "layout","events"], function (ajaxcall, layout,events) {
+define(["ajaxcall", "layout","events","getpage"], function (ajaxcall, layout, events, getpage) {
 	'use strict';
 	var searchButton = document.getElementById("search_button");
 	//Event triggered when someone pushes the search button
 	searchButton.addEventListener("click", function() {
-		var query = document.getElementById("query").value;
-			//Check if there is a query string
-			if (query !== "") {
-				//Get the results form Google Search Api and put them formatted in the web page
-    			ajaxcall.getData({callback:
-    				function(response) { 
-    					if (response === "error") { 
-    						if (document.getElementById("itembox").style.display !== "block") {
-    					    	document.getElementById("itembox").style.display = "block";
-    					    }
-    						document.getElementById("itembox").innerHTML = '<li><h4>Unable to connect to API server</h4>';
-    					}
-    					else if (response === "loading") {
-    						if (document.getElementById("itembox").style.display !== "block") {
-    					    	document.getElementById("itembox").style.display = "block";
-    					    }
-    						document.getElementById("itembox").innerHTML = '<li><h4>Loading...</h4>';
-    					}
-    					else if (response !== "error" && response !== "loading") {
-    						var totalResults = layout.paintIt(response, 1, 1); //paints the web with first results
-    						events.addEvents(totalResults);
-    						document.getElementById("imagebox").style.display = "block";
-    						document.getElementById("itembox").style.display = "block";	
-    		   			}
-    				}, query:query, startIndex:0});
-    		}
-    	});
+		//Check if there is a query string
+		getpage.getPageData(0, 1, 1,function(totalResults){ events.addEvents(totalResults); });
+    });
     	
 });
